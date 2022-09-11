37eebed5a4adc21b4e85e925fcfe0c69fb1ab978
Pratheek G V <109038768+pratheek1904@users.noreply.github.com>
2022-09-11 16:19:59 +0530

    Weather App
    This is a basic weather App, which tells us about the temperature.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Tempapp from './components/Tempapp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tempapp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tempapp.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./Css/style.css";
+const Tempapp = () => {
+  const [city, setcity] = useState(null);
+  const [search, setSearch] = useState("Bangalore");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ca6a2a2ac8a7142ff5e41f35d49605dd`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      // console.log(resJson)
+      setcity(resJson.main);
+    };
+    fetchApi();
+  }, [search]);
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            className="inputField"
+            placeholder="Enter city"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          ></input>
+        </div>
+
+        {!city ? (
+          <p>No Data Found</p>
+        ) : (
+          <div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fa-solid fa-street-view"></i>
+                {search}
+              </h2>
+              <h1 className="temp">{city.temp}°Cel</h1>
+              <h3 className="tempmin_max">
+                Min:{city.temp_min}°Cel |Max:{city.temp_max}°Cel{" "}
+              </h3>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+export default Tempapp;
