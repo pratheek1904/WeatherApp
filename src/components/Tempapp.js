2a1048d834e505cc5dd76185b522f5c536e76809
Pratheek G V <109038768+pratheek1904@users.noreply.github.com>
2022-10-10 14:51:11 +0530

    Weather
    To fetch Weather Data

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -1,19 +1,23 @@
-import React, { useEffect, useState } from "react";
 import "./Css/style.css";
+import React, { useEffect, useState } from "react";
+
+
 const Tempapp = () => {
-  const [city, setcity] = useState(null);
+  const [city, setcity] = useState();
   const [search, setSearch] = useState("Bangalore");
+
 
   useEffect(() => {
     const fetchApi = async () => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ca6a2a2ac8a7142ff5e41f35d49605dd`;
       const response = await fetch(url);
       const resJson = await response.json();
-      // console.log(resJson)
       setcity(resJson.main);
     };
     fetchApi();
   }, [search]);
+
+
   return (
     <>
       <div className="box">
@@ -22,8 +26,8 @@ const Tempapp = () => {
             type="search"
             className="inputField"
             placeholder="Enter city"
-            onChange={(event) => {
-              setSearch(event.target.value);
+            onChange={(e) => {
+            setSearch(e.target.value);
             }}
           ></input>
         </div>
@@ -34,7 +38,7 @@ const Tempapp = () => {
           <div>
             <div className="info">
               <h2 className="location">
-                <i className="fa-solid fa-street-view"></i>
+                 <i className="fa-solid fa-street-view"></i>
                 {search}
               </h2>
               <h1 className="temp">{city.temp}°Cel</h1>
